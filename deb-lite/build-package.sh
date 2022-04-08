2eecdd47acaa358a891f799be723103e61bee0e4
Torsten Martinsen <tma@gatehouse.dk>
2022-04-08 12:15:14 +0200

    Add.

---
+++ deb-lite/build-package.sh
@@ -0,0 +1,26 @@
+#
+# Build debian/ubuntu package using checkinstall.
+#
+#!/bin/bash
+
+if [ "$1" = "" ]
+then
+   echo "Missing source dir argument!"
+   exit 1
+fi
+
+version=`sed -n -e 's/const.*version.*"\(.*\)";/\1/p' $1/release.cpp`
+
+ln -s $1/deb/postinstall-pak
+ln -s $1/deb/preinstall-pak
+ln -s $1/deb/preremove-pak
+ln -s $1/deb/postremove-pak
+ln -s $1/deb/description-pak
+touch install_manifest.txt
+
+make
+
+#run this with sudo. It will remove some of the annoying side effects afterwards.
+sudo checkinstall --install=no --pkgversion=${version} --pkgrelease=${BUILD_NUMBER} --pkglicense=GPL --pkgname=uniproxy --maintainer="Poul Bondo \<pba@gatehouse.dk\>" --provides=uniproxy --requires="openssl" --nodoc --exclude=/home  --backup=no
+
+rm postinstall-pak  postremove-pak  preinstall-pak  preremove-pak description-pak
