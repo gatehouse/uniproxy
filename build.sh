89291316e9a36e42e78df935d155f867551f2bd7
Villy Thomsen <vft@gatehouse.com>
2023-02-15 14:08:53 +0100

    Checkout correct version 1.2.1 of cppcms

--- build.sh
+++ build.sh
@@ -5,16 +5,16 @@ sudo apt update
 sudo apt install -y checkinstall cmake git g++ python libssl-dev libgcrypt20-dev libicu-dev libpcre++-dev libboost-dev
 
 # CppCMS
-VERSION_CPPCMS=1.3.0gh
-VERSION_CPPCMS_GIT=61da055ffeb349b4
+VERSION_CPPCMS=1.2.1
+VERSION_CPPCMS_GIT=v1.2.1
 
 pushd .
-git clone https://github.com/gatehouse/cppcms.git
+git clone https://github.com/artyom-beilis/cppcms.git cppcms
 cd cppcms
 git checkout $VERSION_CPPCMS_GIT
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local
+cmake .. -DCMAKE_INSTALL_PREFIX=/opt/cppcms/$VERSION_CPPCMS
 make -j 4
 sudo make install
 popd
