b46606acaf7df3398501359f020bd45038ff1f2a
Villy Thomsen <vft@gatehouse.com>
2023-02-15 14:56:05 +0100

    Add missing boost libraries to build script

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # Std ubuntu libraris
 sudo apt update
-sudo apt install -y checkinstall cmake git g++ python libssl-dev libgcrypt20-dev libicu-dev libpcre++-dev libboost-dev libsystemd-dev
+sudo apt install -y checkinstall cmake git g++ python libssl-dev libgcrypt20-dev libicu-dev libpcre++-dev libboost-dev libsystemd-dev libboost-filesystem1.71-dev libboost-system1.71-dev libboost-chrono1.71-dev libboost-regex1.71-dev libboost-date-time1.71-dev libboost-iostreams1.71-dev
 
 # CppCMS
 VERSION_CPPCMS=1.2.1
