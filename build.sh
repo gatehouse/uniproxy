f6bb2e935d4566b009fd1c4fdc4e99a18e8f163f
Villy Thomsen <vft@gatehouse.com>
2023-02-15 12:42:44 +0100

    Add build script for codeql

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Std ubuntu libraris
+sudo apt update
+sudo apt install -y checkinstall cmake git g++ python libssl-dev libgcrypt20-dev libicu-dev libpcre++-dev
+
+# CppCMS
+VERSION_CPPCMS=1.3.0gh
+VERSION_CPPCMS_GIT=61da055ffeb349b4
+
+pushd .
+git clone https://github.com/gatehouse/cppcms.git
+cd cppcms
+git checkout $VERSION_CPPCMS_GIT
+mkdir build
+cd build
+cmake .. 
+make
+make install
+popd
+
+# Build
+mkdir build
+cd build
+cmake ..
+make
