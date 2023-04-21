7025361ee128408113690a14483209c2a3baa036
Troels Bak <trb@gatehouse.dk>
2023-04-21 10:28:37 +0200

    Bump release number to v1.8.0

--- release.cpp
+++ release.cpp
@@ -19,4 +19,4 @@
 //====================================================================
 
 const char* release = "GateHouse A/S (C) Uniproxy";
-const char* version = "1.7.7";
+const char* version = "1.8.0";
